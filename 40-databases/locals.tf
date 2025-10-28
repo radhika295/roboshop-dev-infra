2ce63e910e02749a2bec0450dfe87e2fe98a33d6
Radhika <sairadhika.iiitn@gmail.com>
2025-10-28 23:50:06 +0530

    terraform

---
+++ 40-databases/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  ami_id = data.aws_ami.joindevops.id
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+  database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_id.value)[0]
+  common_tags = {
+    Project = var.project_name
+    Environment = var.environment
+    Terraform = true
+  }
+}
