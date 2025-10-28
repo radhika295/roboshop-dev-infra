5a456ad24861043fb0bc2b75803ee9c43841540b
Radhika <sairadhika.iiitn@gmail.com>
2025-10-29 00:02:53 +0530

    terraform

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -22,6 +22,6 @@ data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
 }
 
-data "aws_ssm_parameter" "public_subnet_id" {
-  name  = "/${var.project_name}/${var.environment}/public_subnet_id"
+data "aws_ssm_parameter" "database_subnet_id" {
+  name  = "/${var.project_name}/${var.environment}/database_subnet_id"
 }
