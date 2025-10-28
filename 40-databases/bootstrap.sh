57656651a8c57964498135540db735e80dd0c6ef
Radhika <sairadhika.iiitn@gmail.com>
2025-10-29 00:56:57 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,4 +2,4 @@
 
 dnf install ansible -y
 
-ansible-pull -u https://github.com/radhika295/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/radhika295/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
