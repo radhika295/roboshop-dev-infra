0f97d96dd103bfbe50b729028a2eef359da14777
Radhika <sairadhika.iiitn@gmail.com>
2025-10-30 10:44:37 +0530

    ansible-playbook

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -36,3 +36,44 @@ resource "terraform_data" "mongodb" {
     ]
   }
 }
+
+
+resource "aws_instance" "reddis" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.reddis_sg_id]
+    subnet_id = local.database_subnet_id
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${var.project_name}-${var.environment}-reddis"
+        }
+    )
+}
+
+
+resource "terraform_data" "reddis" {
+  triggers_replace = [
+    aws_instance.reddis.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.reddis.private_ip
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh reddis"
+    ]
+  }
+}
+
