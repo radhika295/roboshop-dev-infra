7ebd740d6ef00683d360b6f96d344519ff6ddfd7
Radhika <sairadhika.iiitn@gmail.com>
2025-10-30 14:35:08 +0530

    ansible-playbook

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -26,6 +26,10 @@ data "aws_ssm_parameter" "redis_sg_id" {
   name = "/${var.project_name}/${var.environment}/redis_sg_id"
 }
 
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project_name}/${var.environment}/rabbitmq_sg_id"
+}
+
 data "aws_ssm_parameter" "database_subnet_id" {
   name  = "/${var.project_name}/${var.environment}/database_subnet_id"
 }
